367f57603343ed5d2f6de9b459049890c3996897
Ashot <tr-ashot.aghajanyan@presight.ai>
2026-02-05 16:13:30 +0400

    impliment get all subjects route

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,13 @@
-import { neon } from "@neondatabase/serverless";
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
 }
 
-const sql = neon(process.env.DATABASE_URL);
+const sql = postgres(process.env.DATABASE_URL, {
+  ssl: "require",
+});
+
 export const db = drizzle(sql);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
+import cors from "cors";
 import express from "express";
+import { subjectsRouter } from "./routes/subjects";
 
 const app = express();
 const PORT = 8000;
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 // JSON middleware
 app.use(express.json());
+app.use("/api/subjects", subjectsRouter);
 
 // Root GET route that returns a short message
 app.get("/", (req, res) => {
